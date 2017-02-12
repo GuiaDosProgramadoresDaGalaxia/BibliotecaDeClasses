48b604bfecd6e72fa07fc3cb19d59d3b72ff974e
Filipe Santiago <porto.santiago@hotmail.com>
2017-02-12 15:51:28 -0300

    Adicionado biblioteca do MongoDB

---
+++ BibliotecaSubeta/BibliotecaSubeta.Mongo/Repositorio/Contexto.cs
@@ -0,0 +1,29 @@
+﻿using BibliotecaSubeta.Mongo.Entidades;
+using MongoDB.Driver;
+using System;
+using System.Configuration;
+
+namespace BibliotecaSubeta.Mongo.Repositorio
+{
+    public class Contexto
+    {
+        private readonly IMongoDatabase database;
+
+        public IMongoDatabase DataBase { get { return database; } }
+
+        public Contexto(string connectionStringNameAppConfig)
+        {
+            var cliente = new MongoClient(ConfigurationManager.ConnectionStrings[connectionStringNameAppConfig].ConnectionString);
+            database = cliente.GetDatabase("GerenciadorMongo");
+        }
+
+        public IMongoCollection<TEntidade> Set<TEntidade>() where TEntidade : class, IEntidade, new()
+        {
+            var entidadeTipo = typeof(TEntidade);
+            var atributo = Attribute.GetCustomAttribute(entidadeTipo, typeof(ColecaoAttribute));
+            var colecao = atributo as ColecaoAttribute;
+
+            return database.GetCollection<TEntidade>(colecao.Nome);
+        }
+    }
+}
