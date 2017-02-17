e5a1d4931e102c8683bdf4d00fd28552cb758dab
Filipe Santiago <porto.santiago@hotmail.com>
2017-02-17 09:25:05 -0300

    Biblioteca unica para o Entity

---
+++ BibliotecaSubeta/BibliotecaSubeta.Entity/Entidades/Entidade.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BibliotecaSubeta.Entity.Entidades
+{
+    public abstract class Entidade<T> : IEntidade<T>
+    {
+        public bool Ativo { get; set; }
+        public string CriadoPor { get; set; }
+        public string ModificadoPor { get; set; }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public T Id { get; set; }
+        object IEntidade.Id
+        {
+            get
+            {
+                return this.Id;
+            }
+        }
+
+        private DateTime? dataCriacao;
+        [DataType(DataType.DateTime)]
+        public DateTime DataCriacao
+        {
+            get { return dataCriacao ?? DateTime.UtcNow; }
+            set { dataCriacao = value; }
+        }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? DataModificacao { get; set; }
+
+        public Entidade()
+        {
+            Ativo = true;
+        }
+    }
+}
---
+++ BibliotecaSubeta/BibliotecaSubeta.Entity/Repositorio/Repositorio.cs
@@ -0,0 +1,220 @@
+﻿using BibliotecaSubeta.Entity.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace BibliotecaSubeta.Entity.Repositorio
+{
+    public class Repositorio<TContexto> : IRepositorio, IDisposable where TContexto : DbContext
+    {
+        protected readonly TContexto contexto;
+
+        public Repositorio(TContexto contexto)
+        {
+            this.contexto = contexto;
+        }
+
+        protected virtual IQueryable<TEntidade> ObterQueryable<TEntidade>(
+        Expression<Func<TEntidade, bool>> filtro = null,
+        Func<IQueryable<TEntidade>, IOrderedQueryable<TEntidade>> ordenarPor = null,
+        int? skip = null,
+        int? take = null,
+        params Expression<Func<TEntidade, object>>[] incluirPropriedades)
+        where TEntidade : class, IEntidade
+        {
+
+            IQueryable<TEntidade> query = contexto.Set<TEntidade>();
+
+            if (filtro != null)
+            {
+                query = query.Where(filtro);
+            }
+
+            if (incluirPropriedades.Length != 0)
+            {
+                query = incluirPropriedades.Aggregate(query, (current, include) => current.Include(include));
+            }
+
+            if (ordenarPor != null)
+            {
+                query = ordenarPor(query);
+            }
+
+            if (skip.HasValue)
+            {
+                query = query.Skip(skip.Value);
+            }
+
+            if (take.HasValue)
+            {
+                query = query.Take(take.Value);
+            }
+
+            return query;
+        }
+
+        #region CRUD    
+        public virtual void Criar<TEntidade>(TEntidade entidade, string criadoPor) where TEntidade : class, IEntidade
+        {
+            entidade.DataCriacao = DateTime.Now;
+            entidade.CriadoPor = criadoPor;
+            contexto.Set<TEntidade>().Add(entidade);
+        }
+
+        public virtual void Atualizar<TEntidade>(TEntidade entidade, string modificadoPor) where TEntidade : class, IEntidade
+        {
+            entidade.DataModificacao = DateTime.Now;
+            entidade.ModificadoPor = modificadoPor;
+            try
+            {
+                contexto.Set<TEntidade>().Attach(entidade);
+                contexto.Entry(entidade).State = EntityState.Modified;
+            }
+            catch (Exception)
+            {
+                //log
+            }
+        }
+
+        public virtual void Deletar<TEntidade>(TEntidade entidade) where TEntidade : class, IEntidade
+        {
+            var dbSet = contexto.Set<TEntidade>();
+            if (contexto.Entry(entidade).State == EntityState.Detached)
+            {
+                dbSet.Attach(entidade);
+            }
+            dbSet.Remove(entidade);
+        }
+
+        public virtual void Deletar<TEntidade>(object id) where TEntidade : class, IEntidade
+        {
+            TEntidade entidade = contexto.Set<TEntidade>().Find(id);
+            Deletar(entidade);
+        }
+
+        public virtual void Registrar<TEntidade>(TEntidade entidade) where TEntidade : class, IEntidade
+        {
+            try
+            {
+                contexto.Set<TEntidade>().Attach(entidade);
+            }
+            catch (Exception)
+            {
+                //log
+            }
+        }
+
+        #endregion
+
+        #region Querys
+        public virtual IEnumerable<TEntidade> ObterTodos<TEntidade>(Expression<Func<TEntidade, bool>> filtro, Func<IQueryable<TEntidade>, IOrderedQueryable<TEntidade>> ordenarPor, int? skip, int? take, params Expression<Func<TEntidade, object>>[] incluirPropriedades) where TEntidade : class, IEntidade
+        {
+            return ObterQueryable<TEntidade>(filtro, ordenarPor, skip, take, incluirPropriedades).ToList();
+        }
+
+        public virtual async Task<IEnumerable<TEntidade>> ObterTodosAsync<TEntidade>(Expression<Func<TEntidade, bool>> filtro, Func<IQueryable<TEntidade>, IOrderedQueryable<TEntidade>> ordenarPor, int? skip, int? take, params Expression<Func<TEntidade, object>>[] incluirPropriedades) where TEntidade : class, IEntidade
+        {
+            return await ObterQueryable<TEntidade>(filtro, ordenarPor, skip, take, incluirPropriedades).ToListAsync();
+        }
+
+        public virtual bool ObterExistente<TEntidade>(Expression<Func<TEntidade, bool>> filtro) where TEntidade : class, IEntidade
+        {
+            return ObterQueryable<TEntidade>(filtro).Any();
+        }
+
+        public virtual Task<bool> ObterExistenteAsync<TEntidade>(Expression<Func<TEntidade, bool>> filtro) where TEntidade : class, IEntidade
+        {
+            return ObterQueryable<TEntidade>(filtro).AnyAsync();
+        }
+
+        public virtual TEntidade ObterPorId<TEntidade>(object id) where TEntidade : class, IEntidade
+        {
+            return contexto.Set<TEntidade>().Find(id);
+        }
+
+        public virtual Task<TEntidade> ObterPorIdAsync<TEntidade>(object id) where TEntidade : class, IEntidade
+        {
+            return contexto.Set<TEntidade>().FindAsync(id);
+        }
+
+        public virtual TEntidade ObterPrimeiro<TEntidade>(Expression<Func<TEntidade, bool>> filtro, Func<IQueryable<TEntidade>, IOrderedQueryable<TEntidade>> ordenarPor, params Expression<Func<TEntidade, object>>[] incluirPropriedades) where TEntidade : class, IEntidade
+        {
+            return ObterQueryable<TEntidade>(filtro, ordenarPor, incluirPropriedades: incluirPropriedades).FirstOrDefault();
+        }
+
+        public virtual async Task<TEntidade> ObterPrimeiroAsync<TEntidade>(Expression<Func<TEntidade, bool>> filtro, Func<IQueryable<TEntidade>, IOrderedQueryable<TEntidade>> ordenarPor, params Expression<Func<TEntidade, object>>[] incluirPropriedades) where TEntidade : class, IEntidade
+        {
+            return await ObterQueryable<TEntidade>(filtro, ordenarPor, incluirPropriedades: incluirPropriedades).FirstOrDefaultAsync();
+        }
+
+        public virtual int ObterQuantidade<TEntidade>(Expression<Func<TEntidade, bool>> filtro) where TEntidade : class, IEntidade
+        {
+            return ObterQueryable<TEntidade>(filtro).Count();
+        }
+
+        public virtual Task<int> ObterQuantidadeAsync<TEntidade>(Expression<Func<TEntidade, bool>> filtro) where TEntidade : class, IEntidade
+        {
+            return ObterQueryable<TEntidade>(filtro).CountAsync();
+        }
+
+
+        public virtual TEntidade Obter<TEntidade>(Expression<Func<TEntidade, bool>> filtro, params Expression<Func<TEntidade, object>>[] incluirPropriedades) where TEntidade : class, IEntidade
+        {
+            return ObterQueryable<TEntidade>(filtro, null, incluirPropriedades: incluirPropriedades).SingleOrDefault();
+        }
+
+        public virtual async Task<TEntidade> ObterAsync<TEntidade>(Expression<Func<TEntidade, bool>> filtro, params Expression<Func<TEntidade, object>>[] incluirPropriedades) where TEntidade : class, IEntidade
+        {
+            return await ObterQueryable<TEntidade>(filtro, null, incluirPropriedades: incluirPropriedades).SingleOrDefaultAsync();
+        }
+        #endregion
+
+        #region Implementacao 'Salvar'
+        public virtual void Salvar()
+        {
+            try
+            {
+                contexto.SaveChanges();
+            }
+            catch (DbEntityValidationException e)
+            {
+                ThrowEnhancedValidationException(e);
+            }
+        }
+
+        public virtual Task SalvarAsync()
+        {
+            try
+            {
+                return contexto.SaveChangesAsync();
+            }
+            catch (DbEntityValidationException e)
+            {
+                ThrowEnhancedValidationException(e);
+            }
+
+            return Task.FromResult(0);
+        }
+
+        protected virtual void ThrowEnhancedValidationException(DbEntityValidationException e)
+        {
+            var errorMessages = e.EntityValidationErrors
+                    .SelectMany(x => x.ValidationErrors)
+                    .Select(x => x.ErrorMessage);
+
+            var fullErrorMessage = string.Join("; ", errorMessages);
+            var exceptionMessage = string.Concat(e.Message, " The validation errors are: ", fullErrorMessage);
+            throw new DbEntityValidationException(exceptionMessage, e.EntityValidationErrors);
+        }
+
+        public void Dispose()
+        {
+            contexto.Dispose();
+        }
+        #endregion
+    }
+}
